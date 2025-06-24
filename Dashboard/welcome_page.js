28cf0e3452f16017b542e8cba91237685191a556
Chibueze Okafor-onyili <Kyrian000>
2025-06-24 13:34:02 +0100

    Fixed dashboard css and js link issues (/)

--- Dashboard/welcome_page.js
+++ Dashboard/welcome_page.js
@@ -22,3 +22,9 @@ new Chart(chart, {
         responsive: true 
     }
 })
+
+// Solidify navbar on scroll
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
